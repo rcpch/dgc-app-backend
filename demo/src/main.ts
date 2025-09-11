32b43a2946ae094c14b573a8cce4a0ef323389ba
Michael Barton <michael.barton@rcpch.ac.uk>
2025-09-11 14:26:40 +0100

    Show sub

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -8,28 +8,54 @@ const config = await client.discovery(
   }
 );
 
-document.getElementById('login_form')?.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  
+const loginForm = document.getElementById('login_form')!;
+const subDisplay = document.getElementById('sub')!;
+
+const token = localStorage['access_token'];
+
+async function login() {
   const code_verifier = client.randomPKCECodeVerifier();
-  const code_challenge = await client.calculatePKCECodeChallenge(code_verifier);
+    const code_challenge = await client.calculatePKCECodeChallenge(code_verifier);
 
-  const state = client.randomState();
+    const state = client.randomState();
 
-  const parameters = {
-    redirect_uri: 'https://dgc-app-backend.localhost/demo/oauth-callback',
-    scope: 'openid profile email',
-    code_challenge,
-    code_challenge_method: 'S256',
-    state
+    const parameters = {
+      redirect_uri: 'https://dgc-app-backend.localhost/demo/oauth-callback',
+      scope: 'openid profile email',
+      code_challenge,
+      code_challenge_method: 'S256',
+      state
+    }
+
+    const authUrl = client.buildAuthorizationUrl(config, parameters);
+
+    sessionStorage['code_verifier'] = code_verifier;
+    sessionStorage['state'] = state;
+
+    window.location.href = authUrl.href;
+}
+
+if (token) {
+  subDisplay.textContent = `Logged in as: ${JSON.parse(atob(token.split('.')[1])).sub}`;
+  loginForm.querySelector("input[type=submit]")!.setAttribute("value", "Logout");
+
+  loginForm.onsubmit = async (e) => {
+    e.preventDefault();
+    
+    delete localStorage['access_token'];
+
+    subDisplay.textContent = "";
+    loginForm.querySelector("input[type=submit]")?.setAttribute("value", "Login");
+
+    loginForm.onsubmit = async (e) => {
+      e.preventDefault(); 
+      await login();
+    }
   }
-
-  const authUrl = client.buildAuthorizationUrl(config, parameters);
-
-  sessionStorage['code_verifier'] = code_verifier;
-  sessionStorage['state'] = state;
-
-  window.location.href = authUrl.href;
-})
-
-console.log(config)
+} else {
+  loginForm?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    await login();
+  });
+}
