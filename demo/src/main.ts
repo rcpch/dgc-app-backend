51f7208b64c84d3edd188c65db5495c289f60c1f
Michael Barton <michael.barton@rcpch.ac.uk>
2025-09-11 12:16:00 +0100

    First half of the oauth dance

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,24 +1,34 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as client from 'openid-client';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.log();
+console.log(import.meta.env.VITE_DEMO_OAUTH_CLIENT_ID);
+console.log(import.meta.env.VITE_DEMO_OAUTH_CLIENT_SECRET);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const config = await client.discovery(
+  new URL(import.meta.env.VITE_DEMO_OAUTH_SERVER),
+  import.meta.env.VITE_DEMO_OAUTH_CLIENT_ID,
+  {
+    client_secret: import.meta.env.VITE_DEMO_OAUTH_CLIENT_SECRET,
+  }
+);
+
+document.getElementById('login_form')?.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  
+  const code_verifier = client.randomPKCECodeVerifier();
+  const code_challenge = await client.calculatePKCECodeChallenge(code_verifier);
+
+  const parameters = {
+    redirect_uri: 'https://dgc-app-backend.localhost/demo/oauth-callback',
+    scope: 'openid profile email',
+    code_challenge,
+    code_challenge_method: 'S256',
+    state: client.randomState()
+  }
+
+  const authUrl = client.buildAuthorizationUrl(config, parameters);
+
+  window.location.href = authUrl.href;
+})
+
+console.log(config)
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-   base: '/demo/',
-   plugins: [
-        {
-      name: 'requestLogger',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-          next();
-        });
-      },
-    },
-   ]
+   base: '/demo/'
 });
