caeb789c305f98598fdbae582fafb07081771d18
Michael Barton <michael.barton@rcpch.ac.uk>
2025-11-20 16:03:14 +0000

    Whoops forgot to load dotenv

--- dgc_app_backend/settings.py
+++ dgc_app_backend/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Has to be before logging_settings as that reads ENABLE_REQUEST_LOGGING
+load_dotenv("envs/.env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
