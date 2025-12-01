2ae4c77efdb26f4ef093cccc1ed32b42ab37f92e
Michael Barton <michael.barton@rcpch.ac.uk>
2025-12-01 15:45:08 +0000

    Move org actions

--- demo/src/components/ChildrenList.tsx
+++ demo/src/components/ChildrenList.tsx
@@ -47,61 +47,13 @@ export function ChildrenList({ organisation }: { organisation: Organisation }) {
     setChildren(children.filter(c => c.id !== id));
   }
 
-  async function onShareOrganisation() {
-    const { invite_id, token } = await shareOrganisation(organisation.id);
-
-    const shareUrl = new URL(window.location.origin);
-    shareUrl.pathname = "/demo/invite";
-    shareUrl.searchParams.set("invite_id", invite_id);
-    shareUrl.searchParams.set("token", token);
-
-    prompt("Share this link:", shareUrl.toString());
-  }
-
-  async function onRemoveUserFromOrganisation(organisation_id: string, user_id: string) {
-    await removeUserFromOrganisation(organisation_id, user_id);
-    window.location.reload();
-  }
-
-  const sharedWithUsers = organisation.users.filter(user => !user.is_current_user);
-
   return <div>
     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div>
         <h3>
           {organisationName(organisation)}
         </h3>
-        {sharedWithUsers.length > 0 &&
-          <ul>
-            {sharedWithUsers.map(user => (
-              <li
-                key={user.email}
-                title={user.email}
-                style={{
-                  listStyleType: 'none',
-                  display: 'inline',
-                  marginRight: '8px'
-                }}>
-                {user.name} {user.is_current_user ? "(You)" : ""}
-                <input
-                  type="button"
-                  value="x"
-                  className="pico-background-red"
-                  style={{
-                    width: '2em',
-                    height: '2em',
-                    padding: '0'
-                  }}
-                  onClick={() => onRemoveUserFromOrganisation(organisation.id, user.id)}
-                />
-              </li>
-            ))}
-          </ul>
-        }
       </div>
-      <button onClick={onShareOrganisation}>
-        Share
-      </button>
     </div>
     <table>
       <thead>
--- demo/src/pages/Organisations.tsx
+++ demo/src/pages/Organisations.tsx
@@ -1,8 +1,25 @@
+import { removeUserFromOrganisation, shareOrganisation } from "../api";
 import { Nav } from "../components/Nav";
 import { useLoggedInAppState } from "../state";
 
 export function Organisations() {
   const appState = useLoggedInAppState();
+
+  async function onShareOrganisation(organisation_id: string) {
+    const { invite_id, token } = await shareOrganisation(organisation_id);
+
+    const shareUrl = new URL(window.location.origin);
+    shareUrl.pathname = "/demo/invite";
+    shareUrl.searchParams.set("invite_id", invite_id);
+    shareUrl.searchParams.set("token", token);
+
+    prompt("Share this link:", shareUrl.toString());
+  }
+
+  async function onRemoveUserFromOrganisation(organisation_id: string, user_id: string) {
+    await removeUserFromOrganisation(organisation_id, user_id);
+    window.location.reload();
+  }
 
   return (
     <div id="app" class="container">
@@ -14,43 +31,47 @@ export function Organisations() {
             <th>ID</th>
             <th>Name</th>
             <th>Users</th>
+            <th></th>
           </tr>
         </thead>
-        {appState.organisations.value.map(organisation => (
-          <tr key={organisation.id}>
-            <td>{organisation.id}</td>
-            <td>{organisation.name}</td>
-            <td>
-              <ul>
-                {organisation.users.map(user => (
-                  <li
-                    key={user.email}
-                    title={user.email}
-                    style={{
-                      listStyleType: 'none',
-                      display: 'inline',
-                      marginRight: '8px'
-                    }}>
-                    {user.email}
-                    {!user.is_current_user && !user.is_creator ? (
-                      <input
-                        type="button"
-                        value="x"
-                        className="pico-background-red"
-                        style={{
-                          width: '2em',
-                          height: '2em',
-                          padding: '0'
-                        }}
-                        onClick={() => onRemoveUserFromOrganisation(organisation.id, user.id)}
-                      />
-                    ) : ''}
-                  </li>
-                ))}
-              </ul>
-            </td>
-          </tr>
-        ))}
+        <tbody>
+          {appState.organisations.value.map(organisation => (
+            <tr key={organisation.id}>
+              <td>{organisation.id}</td>
+              <td>{organisation.name}</td>
+              <td>
+                <ul>
+                  {organisation.users.map(user => (
+                    <li
+                      key={user.email}
+                      title={user.email}
+                    >
+                      {user.email}
+                      {!user.is_creator ? (
+                        <input
+                          type="button"
+                          value="x"
+                          className="pico-background-red"
+                          style={{
+                            width: '2em',
+                            height: '2em',
+                            padding: '0'
+                          }}
+                          onClick={() => onRemoveUserFromOrganisation(organisation.id, user.id)}
+                        />
+                      ) : ''}
+                    </li>
+                  ))}
+                </ul>
+              </td>
+              <td>
+                <button onClick={() => onShareOrganisation(organisation.id)}>
+                  Share
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
