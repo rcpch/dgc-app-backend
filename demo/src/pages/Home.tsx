9416b837653503bc9bad79fba82f8527e7c4b769
Michael Barton <michael.barton@rcpch.ac.uk>
2025-12-01 14:37:55 +0000

    Pull up organisation

--- demo/src/pages/Home.tsx
+++ demo/src/pages/Home.tsx
@@ -1,25 +1,10 @@
-import { useState, useEffect } from 'preact/hooks';
-
-import { testBackend, Organisation, getOrganisations, refreshAccessToken} from '../api';
+import { testBackend, refreshAccessToken} from '../api';
 import { ChildrenList } from '../components/ChildrenList';
 import { Nav } from '../components/Nav';
 import { useLoggedInAppState } from '../state';
 
 export function Home() {
   const appState = useLoggedInAppState();
-
-  const [organisations, setOrganisations] = useState<Organisation[]>([]);
-
-  const authData = appState.authData.value;
-  const accessToken = authData?.access_token;
-
-  useEffect(() => {
-    if(accessToken) {
-      getOrganisations().then(setOrganisations);
-    } else {
-      setOrganisations([]);
-    }
-  }, [accessToken]);
 
   function onTestFormSubmit(e: Event) {
     e.preventDefault();
@@ -34,23 +19,19 @@ export function Home() {
 	return (
 		<div id="app" class="container">
       <Nav appState={appState} />
-      {authData ?
+      <form onSubmit={onTestFormSubmit}>
+        <input type="submit" value="Test Backend" />
+      </form>
+      <form onSubmit={onTestRefreshToken}>
+        <input type="submit" value="Test Refresh Token" />
+      </form>
+      <hr />
+      {appState.organisations.value.map(organisation => (
         <>
-          <form onSubmit={onTestFormSubmit}>
-            <input type="submit" value="Test Backend" />
-          </form>
-          <form onSubmit={onTestRefreshToken}>
-            <input type="submit" value="Test Refresh Token" />
-          </form>
+          <ChildrenList key={organisation.id} organisation={organisation} />
           <hr />
-          {organisations.map(organisation => (
-            <>
-              <ChildrenList key={organisation.id} organisation={organisation} />
-              <hr />
-            </> 
-          ))}
-        </>
-      : ''}
+        </> 
+      ))}
     </div>
 	);
 }
--- demo/src/state.ts
+++ demo/src/state.ts
@@ -3,17 +3,26 @@ import { AuthData, clearAuthData, getAuthData } from "./auth";
 import { useContext } from "preact/hooks";
 import { signal, Signal } from "@preact/signals";
 import { useLocation } from "preact-iso";
+import { getOrganisations, Organisation } from "./api";
 
 export type AppState = {
   authData: Signal<AuthData | undefined>;
+  organisations: Signal<Organisation[]>;
   logout: () => void;
 }
 
 export function createAppState(): AppState {
   const authData = signal<AuthData | undefined>(getAuthData());
+  const organisations = signal<Organisation[]>([]);
+
+  // TODO MRB: catch errors
+  if(authData.value?.access_token) {
+    getOrganisations().then(orgs => organisations.value = orgs);
+  }
 
   return {
     authData: authData,
+    organisations: organisations,
     logout: () => {
       authData.value = undefined;
       clearAuthData();
@@ -21,7 +30,8 @@ export function createAppState(): AppState {
   };
 }
 
-export const AppStateCtx = createContext<AppState>(createAppState());
+// @ts-ignore
+export const AppStateCtx = createContext<AppState>();
 
 export const useAppState = () => {
   return useContext(AppStateCtx);
