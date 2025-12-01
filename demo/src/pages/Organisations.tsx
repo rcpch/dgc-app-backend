ea8b1110936efaa0d56f374269d829c490815364
Michael Barton <michael.barton@rcpch.ac.uk>
2025-12-01 14:50:31 +0000

    wip org list

--- demo/src/pages/Organisations.tsx
+++ demo/src/pages/Organisations.tsx
@@ -7,7 +7,51 @@ export function Organisations() {
   return (
     <div id="app" class="container">
       <Nav appState={appState} />
-      <h2>Organisations Page</h2>
+      <h2>Organisations</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Users</th>
+          </tr>
+        </thead>
+        {appState.organisations.value.map(organisation => (
+          <tr key={organisation.id}>
+            <td>{organisation.id}</td>
+            <td>{organisation.name}</td>
+            <td>
+              <ul>
+                {organisation.users.map(user => (
+                  <li
+                    key={user.email}
+                    title={user.email}
+                    style={{
+                      listStyleType: 'none',
+                      display: 'inline',
+                      marginRight: '8px'
+                    }}>
+                    {user.email}
+                    {!user.is_current_user && !user.is_creator ? (
+                      <input
+                        type="button"
+                        value="x"
+                        className="pico-background-red"
+                        style={{
+                          width: '2em',
+                          height: '2em',
+                          padding: '0'
+                        }}
+                        onClick={() => onRemoveUserFromOrganisation(organisation.id, user.id)}
+                      />
+                    ) : ''}
+                  </li>
+                ))}
+              </ul>
+            </td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
