57e32974cba9653450894eaf519fbe8ec12f64d7
Michael Barton <michael.barton@rcpch.ac.uk>
2025-12-02 11:06:09 +0000

    Logged in test

--- dgc_app_backend/backend/auth.py
+++ dgc_app_backend/backend/auth.py
@@ -46,26 +46,7 @@ def fetch_config(oauthServer: str) -> dict:
       raise ValueError(f"Unknown OAuth server: {oauthServer}")
 
 
-def login_with_third_party_id_token(oauth_server: str, token: str) -> AuthData:
-  config = fetch_config(oauth_server)
-
-  url = f"{config.oauth_server}/.well-known/openid-configuration"
-  oidc_doc = httpx.get(url).json()
-
-  signing_algos = oidc_doc["id_token_signing_alg_values_supported"]
-
-  jwks_client = jwt.PyJWKClient(oidc_doc["jwks_uri"])
-  signing_key = jwks_client.get_signing_key_from_jwt(token)
-
-  claims = jwt.decode(
-      token,
-      signing_key.key,
-      algorithms=signing_algos,
-      audience=config.allowed_client_ids,
-      issuer=config.oauth_server,
-      strict_aud=True
-  )
-
+def create_user(claims) -> AuthData:
   # Extremely important! Don't store the actual user ID in the database as we treat it as a secret
   # to derive the per user encryption key
   user_id = sha_256(claims["sub"])
@@ -100,6 +81,29 @@ def login_with_third_party_id_token(oauth_server: str, token: str) -> AuthData:
     email=claims["email"],
     key=key
   )
+
+
+def login_with_third_party_id_token(oauth_server: str, token: str) -> AuthData:
+  config = fetch_config(oauth_server)
+
+  url = f"{config.oauth_server}/.well-known/openid-configuration"
+  oidc_doc = httpx.get(url).json()
+
+  signing_algos = oidc_doc["id_token_signing_alg_values_supported"]
+
+  jwks_client = jwt.PyJWKClient(oidc_doc["jwks_uri"])
+  signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+  claims = jwt.decode(
+      token,
+      signing_key.key,
+      algorithms=signing_algos,
+      audience=config.allowed_client_ids,
+      issuer=config.oauth_server,
+      strict_aud=True
+  )
+
+  return create_user(claims)
 
 
 def login_with_third_party_access_token(oauth_server: str, token: str) -> AuthData:
--- dgc_app_backend/backend/tests/test_backend.py
+++ dgc_app_backend/backend/tests/test_backend.py
@@ -1,5 +1,25 @@
 import pytest
+from ninja.testing import TestClient
+from ..api import api
+from ..auth import generate_access_token, create_user
+
+@pytest.fixture
+def user_fixture():
+    return create_user({
+        "sub": "test_sub",
+        "name": "Test User",
+        "email": "test@example.com"
+    })
+
 
 @pytest.mark.django_db
-def test_mtest():
-    assert False
+def test_hello(user_fixture):
+    client = TestClient(api)
+    access_token = generate_access_token(user_fixture.sub)
+
+    response = client.get("/hello", headers={
+        "Authorization": f"Bearer {access_token}"
+    })
+
+    assert response.status_code == 200
+    assert response.text == f"\"{user_fixture.name}\""
